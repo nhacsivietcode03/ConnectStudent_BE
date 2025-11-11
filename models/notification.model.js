33bd4800f84624f34de9b29ca024bbf977199f1a
Tran Quoc Huy <pabo011103@gmail.com>
2025-11-11 20:29:42 +0700

    [HuyTQHE173113]Done BE chat box

--- models/notification.model.js
+++ models/notification.model.js
@@ -1,40 +1,58 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const notificationSchema = new mongoose.Schema({
-	recipient: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		required: true
-	},
-	sender: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		required: true
-	},
-	type: {
-		type: String,
-		enum: ['like', 'comment', 'follow_request', 'follow_accept', 'follow_reject'],
-		required: true
-	},
-	post: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Post',
-		required: false
-	},
-	comment: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Comment'
-	},
-	followRequest: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Follow'
-	},
-	read: {
-		type: Boolean,
-		default: false
-	}
-}, { timestamps: true })
+const notificationSchema = new mongoose.Schema(
+    {
+        recipient: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        type: {
+            type: String,
+            enum: [
+                "like",
+                "comment",
+                "follow_request",
+                "follow_accept",
+                "follow_reject",
+                "message",
+            ],
+            required: true,
+        },
+        post: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+            required: false,
+        },
+        comment: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment",
+        },
+        followRequest: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Follow",
+        },
+        conversation: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Conversation",
+        },
+        message: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Message",
+        },
+        read: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestamps: true }
+);
 
-const Notification = mongoose.model('Notification', notificationSchema)
+const Notification = mongoose.model("Notification", notificationSchema);
 
-module.exports = Notification
+module.exports = Notification;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
-const userRouter = require('./user.router')
-const authRouter = require('./auth.router')
-const postRouter = require('./post.router')
-const notificationRouter = require('./notification.router')
-const adminRouter = require('./admin.router')
-const followRouter = require('./follow.router')
+const userRouter = require("./user.router");
+const authRouter = require("./auth.router");
+const postRouter = require("./post.router");
+const notificationRouter = require("./notification.router");
+const adminRouter = require("./admin.router");
+const followRouter = require("./follow.router");
+const chatRouter = require("./chat.router");
 
 module.exports = {
     userRouter,
-	authRouter,
-	postRouter,
-	notificationRouter,
-	adminRouter,
-	followRouter
-}
+    authRouter,
+    postRouter,
+    notificationRouter,
+    adminRouter,
+    followRouter,
+    chatRouter,
+};
